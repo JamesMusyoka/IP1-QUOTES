8ee416abbc39ef0ed1425389a1454d6df52d77c8
James Musyoka <jamesmu475@gmail.com>
2019-01-15 09:24:53 +0300

    Added my quotes to display in the project

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Quotes} from '../quotes';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-quote',
@@ -8,7 +9,13 @@ import {Quotes} from '../quotes';
 })
 
 export class QuotesComponent implements OnInit {
-  quotes = [];
+  quotes = [
+   new Quotes(1, 'It is your determination and persistence that will make you a successful person', ' Pablo Picasso', new Date(2019, 1, 4)),
+   new Quotes(2, 'You can waste your lives drawing lines. Or you can live your life crossing them', '-Ayn Rand', new Date(2019, 2, 3)),
+   new Quotes(3, 'Obstacles are those frightful things you see when you take your eyes off your goal', 'Dale Carne', new Date(2019, 1, 3)),
+   new Quotes(4, 'Formula for success: rise early, work hard, strike oil', 'by -Pearl Buck', new Date(2019, 2, 23)),
+   new Quotes(5, 'Action is the foundational key to all success', 'by George Eliot', new Date(2019, 3, 12))
+  ];
   completeQuote(isComplete, index) {
         if (isComplete) {
             this.quotes.splice(index, 1);
